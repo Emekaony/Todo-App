a49c6e10c9b70a871087046c6a37386ff55e2c3f
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-11-07 16:31:17 -0800

    Just setting things up

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import "package:flutter/material.dart";
+import "package:flutter_app_one/main_screen.dart";
+
+void main() {
+  runApp(const TodoApp());
+}
+
+// this is the general workflow when creating a stateful app in Flutter
+class TodoApp extends StatefulWidget {
+  const TodoApp({super.key});
+
+  @override
+  State<TodoApp> createState() => _TodoAppState();
+}
+
+class _TodoAppState extends State<TodoApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MainScreen(),
+    );
+  }
+}
